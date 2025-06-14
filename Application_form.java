0314c2369083cf88731c8b931c7c44852393fdd5
bhavyasiddi <bhavyasiddi>
2025-06-14 23:28:22 +0530

    done

---
+++ Application_form.java
@@ -0,0 +1,74 @@
+class Application_form
+{
+    public static void main(String[] args)
+{
+    String firstName = "Taylor";
+    String lastName = "Swift";
+    String middleName = "Alison";
+    int age = 35;
+    String occupation = "Musician";
+    String address = "12482 Craig St, Overland Park, KS, 66213, USA";
+    String dob = "12/13/1989";
+    boolean citizenofFilipino = false;
+    boolean married = true;
+    String citizenship = "American";
+    String spouseName = "Travis Klecey";
+    String religion = "Christian";
+    String gender = "Female";
+    int number = 1234567;
+    String fatherName = "Scott Kingsley Swift";
+    String occupationFather = "Stock Broker";
+    int contactNumber = 23456;
+    String motherName = "Andrea Gardener Swift";
+    String motherOccupation = "Marketing Executive";
+    int motherNumber = 34567;
+    String highSchool = "Hendersonville High School";
+    int yearGrad = 2006;
+    double gpa = 4.0;
+    String highestDegree = "Doctorate in Fine arts";
+    String university = "New York University";
+    int graduated = 2022;
+    double cgpa = 3.9;
+    boolean systemPasser = false;
+    boolean beneficiary = true;
+    boolean disability = false;
+    double beneficiaryNum = 234.789;
+
+
+    System.out.println("First Name " + firstName);
+    System.out.println("Middle Name " + middleName);
+    System.out.println("Last Name " + lastName);
+    System.out.println("Age " + age);
+    System.out.println("Occupation " + occupation);
+    System.out.println("Gender " + gender);
+    System.out.println("Citizenship " + citizenofFilipino);
+    System.out.println("Others " + citizenship);
+    System.out.println("Civil Status " + married);
+    System.out.println("Spouse Name " + spouseName);
+    System.out.println("Relogion " + religion);
+    System.out.println("Gender " + gender);
+    System.out.println("Contact Number " + number);
+    System.out.println("-----------------------------------------");
+    System.out.println("Father's Name " + fatherName);
+    System.out.println("Father's Occupation " + occupationFather);
+    System.out.println("Father's Number " + contactNumber);
+    System.out.println("-----------------------------------------");
+    System.out.println("Mother's Name " + motherName);
+    System.out.println("Mother's Occupation " + motherOccupation);
+    System.out.println("Mother's Number " + motherNumber);
+    System.out.println("-----------------------------------------");
+    System.out.println("High School attended " + highSchool);
+    System.out.println("Graduation year of High School " + yearGrad);
+    System.out.println("High School Grade " + gpa);
+    System.out.println("Highest Degree " + highestDegree);
+    System.out.println("University Attended " + university);
+    System.out.println("Year Graduated from Highest Degree " + graduated);
+    System.out.println("Highest Degree Grade " + cgpa);
+    System.out.println("-----------------------------------------");
+    System.out.println("Are you an ALS Passer ? " + systemPasser);
+    System.out.println("Is your family a 4Ps Beneficiary? " + beneficiary);
+    System.out.println("Are you disabled " + disability);
+    System.out.println("4Ps ID no " + beneficiaryNum);
+
+
+}}
